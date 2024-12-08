94681215bd39041cbb04421cd26fa2f96212e2ce
Mrunal <mrunalrawade05@gmail.com>
2024-12-08 11:49:22 +0530

    Add changemode and memegenerator projects

---
+++ React/memegenerator/src/data.js
@@ -0,0 +1,24 @@
+export default {
+  success: true,
+  data: {
+    memes: [
+      {
+        id: "18193649",
+        name: "Drake Hotline Blink",
+        url: "https://th.bing.com/th?id=OIP.8iRMutab62hIdq7nZMToIAHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
+        width: 1200,
+        height: 1200,
+        box_count: 2,
+      },
+
+      {
+        id: "87743020",
+        name: "Two Buttons",
+        url: "https://tse4.mm.bing.net/th/id/OIP.GRUIbzr34_a4V1A9PFijkAHaIY?w=169&h=191&c=7&r=0&o=5&pid=1.7",
+        width: 600,
+        height: 908,
+        box_count: 3,
+      },
+    ],
+  },
+};
