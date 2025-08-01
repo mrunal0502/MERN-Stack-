86ded2a1b554edb553907591e37504b7a435dede
Mrunal <mrunalrawade05@gmail.com>
2025-08-01 13:08:58 +0530

    Upgrade Permalist to multi-todo list

--- Assignments/Backend/PermalistProject/index.js
+++ Assignments/Backend/PermalistProject/index.js
@@ -19,14 +19,26 @@ const db = new pg.Client({
 db.connect();
 
 let items = [];
+let monthitems = [];
+let yearitems = [];
 
 app.get("/", async (req, res) => {
   try {
     const result = await db.query("SELECT * FROM items ORDER BY id ASC");
     items = result.rows;
+    const monthResult = await db.query("SELECT * FROM month ORDER BY id ASC");
+    monthitems = monthResult.rows;
+    console.log("Month items:", monthitems);
+    const yearResult = await db.query("SELECT * FROM year ORDER BY id ASC");
+    console.log("Year items:", yearResult.rows);
+    yearitems = yearResult.rows;
     res.render("index.ejs", {
       listTitle: "Today",
       listItems: items,
+      listTitlemonth: "This month",
+      listMonthItems: monthitems,
+      listTitleyear: "This year",
+      listYearItems: yearitems,
     });
   } catch (err) {
     console.error("Error fetching items:", err);
@@ -35,40 +47,74 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/add", async (req, res) => {
-  const item = req.body.newItem;
-  await db.query(
-    "INSERT INTO items (title) VALUES ($1)",
-    [item],
-    (err, result) => {
-      if (err) {
-        console.error("Error inserting item:", err);
-      } else {
-        console.log("Item inserted successfully");
-      }
-    }
-  );
-  res.redirect("/");
-});
-
-app.post("/edit", (req, res) => {
-  const id = req.body.updatedItemId;
-  console.log("Editing item with ID:", id);
-  const updateitem = req.body.updatedItemTitle;
-  console.log("Updated item:", updateitem);
   try {
-    db.query("UPDATE items SET title = $1 WHERE id=$2", [updateitem, id]);
+    const item = req.body.newItem;
+    if (item) {
+      await db.query("INSERT INTO items (title) VALUES ($1)", [item]);
+    }
+    const mitem = req.body.newItemMonth;
+    if (mitem) {
+      await db.query("INSERT INTO month (mitem) VALUES ($1)", [mitem]);
+    }
+    const yitem = req.body.newItemYear;
+    if (yitem) {
+      await db.query("INSERT INTO year (yitem) VALUES ($1)", [yitem]);
+    }
     res.redirect("/");
   } catch (err) {
-    console.error("Error updating item:", err);
+    console.error("Error inserting item:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/edit", async (req, res) => {
+  try {
+    const id = req.body.updatedItemId;
+    const item = req.body.updatedItemTitle;
+    if (id) {
+      await db.query("UPDATE items SET title=$1 WHERE id=$2", [item, id]);
+    }
+    const mid = req.body.updatedItemIdMonth;
+    const mitem = req.body.updatedItemTitlem;
+    if (mid) {
+      await db.query("UPDATE month SET mitem=$1 WHERE id=$2", [mitem, mid]);
+    }
+    const yid = req.body.updatedItemIdYear;
+    const yitem = req.body.updatedItemTitley;
+    if (yid) {
+      await db.query("UPDATE year SET yitem=$1 WHERE id=$2", [yitem, yid]);
+    }
+    res.redirect("/");
+  } catch (err) {
+    console.log("Error in editing query", err);
     res.status(500).send("Internal Server Error");
   }
 });
 
 app.post("/delete", (req, res) => {
-  const id = req.body.deleteItemId;
-  console.log("Deleting item with ID:", id);
-  db.query("DELETE FROM items WHERE id=$1", [id]);
-  res.redirect("/");
+  try {
+    const id = req.body.deleteItemId;
+    if (id != null) {
+      console.log("Deleting item with ID:", id);
+      db.query("DELETE FROM items WHERE id=$1", [id]);
+    }
+
+    const mid = req.body.deleteItemIdm;
+    if (mid != null) {
+      console.log("Deleting month item with ID", mid);
+      db.query("DELETE FROM month WHERE id=$1", [mid]);
+    }
+
+    const yid = req.body.deleteItemIdy;
+    if (yid != null) {
+      console.log("Deleting year item with ID:", yid);
+      db.query("DELETE FROM year WHERE id=$1", [yid]);
+    }
+    res.redirect("/");
+  } catch (err) {
+    console.log("Error in deleting query", err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.listen(port, () => {
