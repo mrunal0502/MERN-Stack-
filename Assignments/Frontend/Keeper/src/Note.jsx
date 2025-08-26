f94e8fc93bdf211ecb0febe1ba20d05f9ee4f746
Mrunal <mrunalrawade05@gmail.com>
2025-08-26 08:13:44 +0530

    Add Frontend - Keeper

---
+++ Assignments/Frontend/Keeper/src/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Note() {
+  return (
+    <div className="note">
+      <h1>This the note title</h1>
+      <p>This is the content</p>
+    </div>
+  );
+}
